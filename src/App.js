b25017926ab670ef9e9c6d0108c7b02240a15659
周浩鹏 <zhouhaopeng@banggood.com>
2018-10-09 18:25:39 +0800

    测试使用ele

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
 import logo from './logo.svg';
+import { Button, Layout } from 'element-react';
+
+import 'element-theme-default';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Layout.Row>
+            <Layout.Col span="24">
+                <div className="grid-content bg-purple-dark">sda</div>
+                <Button type="primary">Hello</Button>
+                <i className="el-icon-edit"></i>
+            </Layout.Col>
+        </Layout.Row>
+
+        
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
