39e0d94fd7c2b88f17c47791487370b5ab5253ec
周浩鹏 <zhouhaopeng@banggood.com>
2018-10-15 19:04:53 +0800

    路由嵌套初步

---
+++ src/App2.js
@@ -0,0 +1,107 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "./redux/action-creators";
+
+// import ReactDOM from 'react-dom';
+import logo from "./logo.svg";
+import { Button, Layout } from "element-react";
+// import echarts from "echarts";
+
+import "element-theme-default";
+import "./App.css";
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props);
+    }
+
+    onTimeButtonClick(delay) {
+        // 这个按钮处理器在用户的点击事件后会分发一个 action。
+        // 我们在这里会使用一个 Connect 提供的分发函数,
+        // 也有很多其他的调用被绑定到分发器的 actionCreator 的方式,
+        // 这种方式提供了第二个 Connect 的参数：
+        // https://github.com/reactjs/react-redux/blob/master/docs/api.md#connectmapstatetoprops-mapdispatchtoprops-mergeprops-options
+        // 被传到 actionCreators.getTime 的 delay 值是为了在我们能得到当前时间之前模拟异步的工作,
+        // 试着修改这个值来正确影响我们的 UI
+        this.props.dispatch(actionCreators.getTime(delay));
+    }
+
+    render() {
+        // 因为 Connect 我们能够通过 props 取到特定的数据
+        var { frozen, time, reduxState } = this.props;
+        var attrs = {};
+        const DELAY = 500; // 毫秒
+
+        if (frozen) {
+            attrs = {
+                disabled: true
+            };
+        }
+
+        return (
+            <div className="App">
+                <Layout.Row>
+                    <Layout.Col span="24">
+                        <div className="grid-content bg-purple-dark">sda</div>
+                        <Button type="primary">Hello22222222</Button>
+                        <i className="el-icon-edit" />
+                    </Layout.Col>
+                </Layout.Row>
+
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">
+                    To get started, edit <code>src/App.js</code> and save to
+                    reload.
+                </p>
+
+                <div>
+                    <h1>Provider and connect example</h1>
+                    <span>
+                        <b>What time is it?</b>{" "}
+                        {time ? `It is currently ${time}` : "No idea yet..."}
+                    </span>
+                    <br /> <br />
+                    <i>
+                        When clicking the button below, the time will be
+                        provided after a {DELAY}
+                        ms delay.
+                        <br />
+                        Try to change this value (in{" "}
+                        <b>src/home.jsx - line 95</b>) to verify that the delay
+                        given correctly impacts our UI.
+                    </i>
+                    <br />
+                    {/* 这里注册按钮的 "onClick" 句柄: */}
+                    <button
+                        {...attrs}
+                        onClick={() => this.onTimeButtonClick(DELAY)}
+                    >
+                        Get time!
+                    </button>
+                    <pre>
+                        redux state = {JSON.stringify(reduxState, null, 2)}
+                    </pre>
+                </div>
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = (state /*, props*/) => {
+    return {
+        frozen: state._time.frozen,
+        time: state._time.time,
+        // 像 (reduxState: state) 这样提供整个 state 是一种不好的实现,
+        // 我们在这里这样写是为了让大家能看到我们页面字符串化的结果。更多信息请访问以下链接:
+        // https://github.com/reactjs/react-redux/blob/master/docs/api.md#inject-dispatch-and-every-field-in-the-global-state
+        reduxState: state
+    };
+};
+
+const ConnectedHome = connect(mapStateToProps)(App);
+
+export default ConnectedHome;
--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import createStore from './redux/create-store'
-import { Provider } from 'react-redux'
-import './normalize.css'
-import './index.css';
-import './assets/less/font.less';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import createStore from "./redux/create-store";
+import { Provider } from "react-redux";
+import "./normalize.css";
+import "./index.css";
+import "./assets/less/font.less";
+import App from "./App";
+import App2 from "./App2";
+import registerServiceWorker from "./registerServiceWorker";
 
-const store = createStore()
+const store = createStore();
 
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <BrowserRouter>
+            <div>
+                <Route path="/" component={App} />
+                <Route path="/haha" component={App2} />
+            </div>
+        </BrowserRouter>
     </Provider>,
-document.getElementById('root'));
+    document.getElementById("root")
+);
 registerServiceWorker();
-
-
