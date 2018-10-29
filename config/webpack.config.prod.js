bc8423f7bee9bdb4d949c6464057d9d61e29a971
herbertchow <herbertwork@163.com>
2018-10-29 18:01:06 +0800

    配置生产环境兼容antd打包

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -208,6 +208,9 @@ module.exports = {
                     },
                     {
                       loader: require.resolve('less-loader'),
+                      options: {
+                        javascriptEnabled: true
+                      }
                     },
                   ],
                 },
