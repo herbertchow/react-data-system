290f8046944229773991ed2096348be5388ccd9e
herbertchow <herbertwork@163.com>
2018-10-30 18:34:16 +0800

    加入axios，改变路由方式，标题

--- src/components/layout/Hbheader.js
+++ src/components/layout/Hbheader.js
@@ -11,6 +11,7 @@ class Hbheader extends Component {
             <div className="hb-header">
                 <div className="hb-logo">
                     <img src={logo} className="App-logo" alt="logo" />
+                    <span className="App-logo-text">赫伯特数据</span>
                 </div>
                 <div className="top-menu-wrap">
                     <Menu
@@ -20,13 +21,13 @@ class Hbheader extends Component {
                         defaultSelectedKeys={["1"]}
                     >
                         <Menu.Item key="1">
-                            <Link to="/">首页</Link>
+                            <Link replace to="/">首页</Link>
                         </Menu.Item>
                         <Menu.Item key="2">
-                            <Link to="/FlowAnalysis">流量</Link>
+                            <Link replace to="/FlowAnalysis">流量</Link>
                         </Menu.Item>
                         <Menu.Item key="3">
-                            <Link to="/ReportSheet">报表</Link>
+                            <Link replace to="/ReportSheet">报表</Link>
                         </Menu.Item>
                     </Menu>
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 import createStore from "./redux/create-store";
 import { Provider } from "react-redux";
 import "./normalize.css";
