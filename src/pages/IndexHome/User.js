8f94edafc33f798ddc68a994605168e3e82aed7c
herbertchow <>
2018-11-27 22:26:51 +0800

    测试数据

--- src/pages/IndexHome/User.js
+++ src/pages/IndexHome/User.js
@@ -1,40 +1,67 @@
-import React, { Component } from "react";
-import "./User.less";
-import LineChart from "@appSrc/components/LineChart";
+import React, { Component } from 'react';
+import './User.less';
+import LineChart from '@appSrc/components/LineChart';
 
 class User extends Component {
-    componentDidMount(){
-        setInterval(()=>{
-            console.log(111)
-        },2000)
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: {
+        datas: [
+          {
+            name: '邮件营销',
+            type: 'line',
+            areaStyle: {},
+            data: [120, 132, 101, 134, 90, 230, 210]
+          },
+          {
+            name: '联盟广告',
+            type: 'line',
+            areaStyle: {},
+            data: [220, 182, 191, 234, 290, 330, 310]
+          }
+        ],
+        categories: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+      }
+    };
+  }
 
-    render() {
-        let data = {
-            datas:[{
-                name: '邮件营销',
-                type: 'line',
-                areaStyle: {},
-                data: [120, 132, 101, 134, 90, 230, 210]
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        data: {
+          datas: [
+            {
+              name: '邮件营销',
+              type: 'line',
+              areaStyle: {},
+              data: [Math.random()*220, 132, 101, 134, 90, 230, 210]
             },
             {
-                name: '联盟广告',
-                type: 'line',
-                areaStyle: {},
-                data: [220, 182, 191, 234, 290, 330, 310]
-            }],
-            categories:['周一', '周二', '周三', '周四', '周五', '周六', '周日'],
-        };
+              name: '联盟广告',
+              type: 'line',
+              areaStyle: {},
+              data: [Math.random()*120, 182, 191, 234, 290, 330, 310]
+            }
+          ],
+          categories: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
+        }
+      });
+    },  1000);
+  }
 
-        return (
-            <div className="index-user">
-                用户
-                <LineChart data={data}/>
-                <br/>
-                <LineChart data={data}/>
-            </div>
-        );
-    }
+  render() {
+    let data = this.state.data;
+
+    return (
+      <div className="index-user">
+        用户
+        <LineChart data={data} />
+        <br />
+        <LineChart data={data} />
+      </div>
+    );
+  }
 }
 
 export default User;
