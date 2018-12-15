19e43b31111aa740ecee54b7a66f81fdc9f0a8be
herbertchow <herbertwork@163.com>
2018-12-15 15:05:21 +0800

    加入拖拽排序demo

--- src/pages/FlowAnalysis/Funnel.js
+++ src/pages/FlowAnalysis/Funnel.js
@@ -1,16 +1,110 @@
-import React, { Component } from "react";
-import "./Funnel.less";
+import React, { Component } from 'react';
+import './Funnel.less';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
+// fake data generator
+const getItems = count =>
+  Array.from({ length: count }, (v, k) => k).map(k => ({
+    id: `item-${k}`,
+    content: `item ${k}`
+  }));
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+});
 
 class Funnel extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: getItems(30)
+    };
+    this.onDragEnd = this.onDragEnd.bind(this);
+  }
 
-    render() {
-
-        return (
-            <div className="funnel">
-                漏斗
-            </div>
-        );
+  onDragEnd(result) {
+    // dropped outside the list
+    if (!result.destination) {
+      return;
     }
+
+    const items = reorder(
+      this.state.items,
+      result.source.index,
+      result.destination.index
+    );
+
+    this.setState({
+      items
+    });
+  }
+
+  render() {
+    return (
+      <div className='funnel'>
+        单列拖拽排序
+        <br />
+        采用react-beautiful-dnd这个库，亲测 一般电脑的性能测试 <br />
+        几乎没有肉眼可见卡顿感 大约在100-300条数据一页达到顶峰 <br />
+        对于条数要求比较高的用户 可能不太适合这个库
+        <DragDropContext onDragEnd={this.onDragEnd}>
+          <Droppable droppableId='droppable'>
+            {(provided, snapshot) => (
+              <div
+                ref={provided.innerRef}
+                style={getListStyle(snapshot.isDraggingOver)}
+              >
+                {this.state.items.map((item, index) => (
+                  <Draggable key={item.id} draggableId={item.id} index={index}>
+                    {(provided, snapshot) => (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        style={getItemStyle(
+                          snapshot.isDragging,
+                          provided.draggableProps.style
+                        )}
+                      >
+                        {item.content}
+                      </div>
+                    )}
+                  </Draggable>
+                ))}
+                {provided.placeholder}
+              </div>
+            )}
+          </Droppable>
+        </DragDropContext>
+      </div>
+    );
+  }
 }
 
 export default Funnel;
